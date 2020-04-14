e9d8de05a97e347f878a47579b28afb63778bc26
zengrixi <zengrixi012739@gmail.com>
2020-04-14 14:27:40 +0800

    修改消息窗体为最新版

--- include/gui/messagebox.h
+++ include/gui/messagebox.h
@@ -22,13 +22,15 @@ class MessageBox;
  * 自定义消息对话框，提供普通消息对话框和确认消息对话框，使用方式如下:
  *     MessageBox::message("Your Message")
  *     if (MessageBox::confirm("确定要删除 XX 吗?")) {}
+ *      if (messageBox::window(QWidget *)) {}
  */
 class MessageBox : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit MessageBox(const QString &message, bool cancel_button_visible = true);
+    MessageBox(const QString &message, bool cancel_button_visible = true); // 创建对话框
+    MessageBox(QWidget *w, bool cancel_button_visible = true);             // 创建窗体
     ~MessageBox();
 
     /**
@@ -80,8 +82,38 @@ public:
         bool border_image_horizontal_stretch = false,
         bool border_image_vertical_stretch = false);
 
+    /**
+     * @brief 显示模态确认对话框，会阻塞当前线程。
+     *        消息内容是窗体
+     *        可以指定宽度和高度，如果宽度和高度小于 sizeHint() 给出的大小，则使用 sizeHint()，
+     *        如果大于 sizeHint() 则使用我们指定的宽度和高度
+     *
+     * @param msg    要显示的消息
+     * @param width  消息对话框的宽度
+     * @param height 消息对话框的高度
+     * @param window_paddings  // 参考 TopWindow 的构造函数参数说明，一下几个参数也是一样的
+     * @param border_image_borders
+     * @param border_image_path
+     * @param border_image_horizontal_stretch
+     * @param border_image_vertical_stretch
+     * @return 点击确认返回 true，点击取消返回 false
+     */
+    static bool window(QWidget *w,
+        int width = 300,
+        int height = 150,
+        const QMargins &window_paddings = QMargins(13, 13, 13, 13),
+        const QMargins &border_image_borders = QMargins(18, 18, 18, 18),
+        const QString &border_image_path = QString(":/img/top-window/colorful-border.png"),
+        bool border_image_horizontal_stretch = false,
+        bool border_image_vertical_stretch = false);
+
 private:
-    static void setWindowForMessageBox(TopWindow *window, int width, int height); // 设置 TopWindow 的属性
+    static void setMessageBoxForTopWindow(TopWindow *window, int width, int height); // 设置 TopWindow 的属性
+
+    /**
+     * @brief 窗体初始化工作
+     */
+    inline void init();
 
     Ui::MessageBox *ui;
     bool result_ = false;
--- src/gui/messagebox.cpp
+++ src/gui/messagebox.cpp
@@ -4,25 +4,18 @@
 
 MessageBox::MessageBox(const QString &message, bool cancel_button_visible) : ui(new Ui::MessageBox)
 {
-    ui->setupUi(this);
+    init();
     ui->message_label->setAttribute(Qt::WA_TransparentForMouseEvents);
     ui->message_label->setText(message);
     ui->cancel_button->setVisible(cancel_button_visible);
+}
 
-    setAttribute(Qt::WA_StyledBackground);
-    setStyleSheet(".MessageBox { background: white; }");
-
-    // 点击取消按钮关闭窗口
-    connect(ui->cancel_button, &QPushButton::clicked, [this] {
-        result_ = false;
-        TopWindow::findWindow(this)->close();
-    });
-
-    // 点击确定按钮关闭窗口
-    connect(ui->ok_button, &QPushButton::clicked, [this] {
-        result_ = true;
-        TopWindow::findWindow(this)->close();
-    });
+MessageBox::MessageBox(QWidget *w, bool cancel_button_visible) : ui(new Ui::MessageBox)
+{
+    init();
+    // 替换中心窗体为传入的窗体
+    ui->gridLayout->replaceWidget(ui->message_label, w);
+    ui->cancel_button->setVisible(cancel_button_visible);
 }
 
 MessageBox::~MessageBox()
@@ -43,7 +36,7 @@ void MessageBox::message(const QString &msg,
     MessageBox *box = new MessageBox(msg, false);
     TopWindow *window =
         new TopWindow(box, window_paddings, border_image_borders, border_image_path, border_image_horizontal_stretch, border_image_vertical_stretch);
-    MessageBox::setWindowForMessageBox(window, width, height);
+    MessageBox::setMessageBoxForTopWindow(window, width, height);
     window->setAttribute(Qt::WA_DeleteOnClose);
     window->show();
 }
@@ -60,17 +53,56 @@ bool MessageBox::confirm(const QString &msg,
     // 使用自定义窗口
     MessageBox *box = new MessageBox(msg, true);
     TopWindow window(box, window_paddings, border_image_borders, border_image_path, border_image_horizontal_stretch, border_image_vertical_stretch);
-    MessageBox::setWindowForMessageBox(&window, width, height);
+    MessageBox::setMessageBoxForTopWindow(&window, width, height);
     window.showModal();
 
     return box->result_;
 }
 
-void MessageBox::setWindowForMessageBox(TopWindow *window, int width, int height)
+bool MessageBox::window(QWidget *w,
+    int width,
+    int height,
+    const QMargins &window_paddings,
+    const QMargins &border_image_borders,
+    const QString &border_image_path,
+    bool border_image_horizontal_stretch,
+    bool border_image_vertical_stretch)
+{
+    // 使用自定义窗口
+    MessageBox *box = new MessageBox(w);
+    TopWindow window(box, window_paddings, border_image_borders, border_image_path, border_image_horizontal_stretch, border_image_vertical_stretch);
+    window.setTitle(w->windowTitle());
+    MessageBox::setMessageBoxForTopWindow(&window, width, height);
+    window.showModal();
+
+    return box->result_;
+}
+
+void MessageBox::setMessageBoxForTopWindow(TopWindow *window, int width, int height)
 {
     window->setTitleBarVisible(false);
     window->setResizable(false);
     window->setWindowFlags(Qt::Dialog | Qt::Popup | Qt::FramelessWindowHint);
     window->setWindowModality(Qt::ApplicationModal);
     window->resize(width, height);
+}
+
+inline void MessageBox::init()
+{
+    ui->setupUi(this);
+
+    setAttribute(Qt::WA_StyledBackground);
+    setStyleSheet(".MessageBox { background: white; }");
+
+    // 点击取消按钮关闭窗口
+    connect(ui->cancel_button, &QPushButton::clicked, [this] {
+        result_ = false;
+        TopWindow::findWindow(this)->close();
+    });
+
+    // 点击确定按钮关闭窗口
+    connect(ui->ok_button, &QPushButton::clicked, [this] {
+        result_ = true;
+        TopWindow::findWindow(this)->close();
+    });
 }
